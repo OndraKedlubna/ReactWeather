12cafd5ef4600e00f99c4aea0c1834183f530fb1
ondra Ke <ondra.to.stejne.necte@seznam.cz>
2019-02-20 21:18:44 +0100

    add foundation and jquery

---
+++ app/components/Example.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var {Link} = require('react-router');
+
+var Example = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">Example component</h1>
+            <p>Examples to try:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Karlovy Vary'>Karlovy Vary</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Praha'>Praha</Link>
+                </li>
+            </ol>
+        </div>
+    )
+};
+
+
+module.exports = Example;
